779233d4dff33888923782494877cfccb9775cec
Artem Diashkin <nemrosim1988@gmail.com>
2020-04-20 19:41:39 +0300

    app state bug fix

--- lib/models/store/app_state.dart
+++ lib/models/store/app_state.dart
@@ -14,4 +14,23 @@ class AppState {
       loginResponse: authState ?? this._loginResponse,
     );
   }
+
+  LoginResponse get authState => _loginResponse;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is AppState &&
+          runtimeType == other.runtimeType &&
+          _loginResponse == other._loginResponse;
+
+  @override
+  int get hashCode => _loginResponse.hashCode;
+
+  @override
+  String toString() {
+    return 'AppState{'
+        '\n_authState: '
+        '\n$_loginResponse}';
+  }
 }
